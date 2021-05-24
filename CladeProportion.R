8c79397f3269fd3cdf4ddac7e408462ca1ea6241
aacapoferri <75907573+aacapoferri@users.noreply.github.com>
2021-05-24 12:50:05 -0400

    Update CladeProportion.R

--- CladeProportion.R
+++ CladeProportion.R
@@ -11,7 +11,7 @@
 #Make sure you have installed ggplot2
 
 library(ggplot2)
-cladeprop<-read.csv("~/Desktop/COVID/R scripts for analysis/cladeprop.csv")
+cladeprop<-read.csv("~/Desktop/cladeprop.csv")
 gg<-ggplot(cladeprop, aes(x=Month, y=Percentage, fill=Clade)) + 
   geom_area(alpha=0.60 , size=0, colour=("black")) + theme_classic()
 gg_new<-gg + scale_fill_brewer(palette="Set1") + theme(axis.text.y=element_text(size=12, face="bold"), axis.text.x=element_text(size=12, face="bold"), axis.title=element_text(size=12, face="bold"), axis.text = element_text(size = 12, face="bold")) + scale_x_continuous(breaks = 1:11, labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov")) + labs(y="Proportion of Sequences")
