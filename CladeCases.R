7508c9139f389831ee0e34b3e7925fd863243121
aacapoferri <75907573+aacapoferri@users.noreply.github.com>
2021-05-24 10:36:48 -0400

    Update CladeCases.R

--- CladeCases.R
+++ CladeCases.R
@@ -1,3 +1,15 @@
+###################################################
+#              Generating Clade Proportions       #
+#                 Version 1.0 (Jan 2021)          #
+#             Written by Adam A. Capoferri        #               
+###################################################
+
+#This script was written to illustrate the GISAID clade of SARS-CoV-2 proportions overtime. But could be used for PANGO lineages as well. 
+
+#Also make sure you set to the correct directory first!
+
+#Make sure you have installed ggplot2
+
 library(ggplot2)
 cladeprop<-read.csv("~/Desktop/cladecases.csv")
 gg<-ggplot(cladeprop, aes(x=Month, y=Percentage, fill=Clade), yaxp=c(0, 5000000, 200000)) + 
