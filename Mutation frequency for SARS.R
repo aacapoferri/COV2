3c17bbbcbb1b6766c7cb3cf17882113237d4c4e5
aacapoferri <75907573+aacapoferri@users.noreply.github.com>
2021-05-24 12:48:16 -0400

    Update Mutation frequency for SARS.R

--- Mutation frequency for SARS.R
+++ Mutation frequency for SARS.R
@@ -17,7 +17,7 @@ library(ggrepel)
 library(ggeasy)
 
 #Read file for analysis: Ensure title heads from A to D are: Position, Mutation, Frequency, Gene. Column A is position. The position can be arbitrary or reflect the true genomic position. Column B is the name of the mutation. Column C is the frequency of the mutation. Column D is the name of the gene
-sarscov2<-read.csv("data.csv") 
+sarscov2<-read.csv("MutationFrequency.csv") 
 
 #This will create a generic plot of Mutation Frequency vs position. Having the column of Gene, will color the points based on the Gene 
 ggplot(sarscov2, aes(x=Position, y=Frequency))+geom_point() + geom_point(aes(col=Gene), size=1.5)
@@ -27,7 +27,7 @@ sarscov2_sub<-sarscov2[sarscov2$Frequency>0.05,]
 sarscov2$Mutation<-ifelse(sarscov2$Frequency>0.05, sarscov2_sub$Mutation, "")
 
 #Setting the base plot and changes the labels. For title change as necessary.
-gg<-ggplot(sarscov2, aes(x=Position, y=Frequency)) + geom_point(aes(col=Gene), size=1) + labs(title="Clade G Phase 2", y="Relative Mutation Proportion", x="SARS-CoV-2 Genome") 
+gg<-ggplot(sarscov2, aes(x=Position, y=Frequency)) + geom_point(aes(col=Gene), size=1) + labs(title="Clade GH", y="Relative Mutation Proportion", x="SARS-CoV-2 Genome") 
 
 #Plot the text and label that repels (ggrepel) each other. It also keeps the gene order correct
 nextgg<-gg+geom_text_repel(max.overlaps = Inf, min.segment.length = 0, aes(label=Mutation,), size=3, data=sarscov2_sub) + scale_color_discrete(breaks=c("orf1a", "orf1b", "S gene", "E gene", "M gene", "N gene")) + theme(axis.text.y=element_text(size=20, face="bold"), axis.text.x=element_text(size=15, face="bold"), axis.title=element_text(size=20, face="bold"), axis.text = element_text(size = 20, face="bold"))
