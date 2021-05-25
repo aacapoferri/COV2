93b634c7b3b619ad965ea0fa49cedf73379481e0
Adam A Capoferri <75907573+aacapoferri@users.noreply.github.com>
2021-05-25 10:03:27 -0400

    Update Deaths by Region.R

--- Deaths by Region.R
+++ Deaths by Region.R
@@ -1,7 +1,9 @@
+#This will graph the number of reported deaths. Currently set as a discrete time-scale but can be altered to continuous.
+
 library(ggplot2)
 library(dplyr)
-blah<-read.csv("~/Desktop/deathsbyregion.csv")
-gg<-ggplot(data=blah, aes(x=Month, y=Sample, group=Region)) + geom_line(aes(color=Region))
+data<-read.csv("~/Desktop/deathsbyregion.csv")
+gg<-ggplot(data=data, aes(x=Month, y=Sample, group=Region)) + geom_line(aes(color=Region))
 gg_regioncases<-gg + ylim(0, 80000) + scale_color_manual(values=c("#FF0000", "#0000ff", "#FF00FF", "#008080")) + theme(axis.text.y=element_text(size=10), axis.text.x=element_text(size=10), axis.title=element_text(size=10), axis.text = element_text(size = 10))
 p<- gg_regioncases + theme_classic() + ylab("Number of individuals") + theme(legend.title=element_blank()) + scale_x_discrete(name="Month", limits=c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"))
 p
