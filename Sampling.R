5cf75efa06f348d74b682e8d39f3337481baec8e
Adam A Capoferri <75907573+aacapoferri@users.noreply.github.com>
2021-05-25 09:57:04 -0400

    Add files via upload

---
+++ Sampling.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+library(dplyr)
+blah<-read.csv("~/Desktop/sampling.csv")
+gg<-ggplot(data=blah, aes(x=Month, y=Sample, group=Subset)) + geom_line(aes(color=Subset))
+gg_new<-gg + ylim(0, 2200000) + scale_color_manual(values=c("#FF0000", "#000000", "#0000ff")) + theme(axis.text.y=element_text(size=10), axis.text.x=element_text(size=10), axis.title=element_text(size=10), axis.text = element_text(size = 10))
+p<- gg_new + theme_classic() + scale_x_discrete(name="Month", limits=c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug")) + ylab("Number of individuals") + theme(legend.title=element_blank())
+p
