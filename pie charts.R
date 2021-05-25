92a7ec71c6e85726d110b8a0a150ac6c1931206d
Adam A Capoferri <75907573+aacapoferri@users.noreply.github.com>
2021-05-25 10:10:55 -0400

    Update pie charts.R

--- pie charts.R
+++ pie charts.R
@@ -13,8 +13,8 @@ library(dplyr)
 df <- data.frame(group = c("Northeast", "South", "Midwest", "West"), value = c(14.8, 18.6, 19.0, 47.6))
 head(df)
 
-bp<-ggplot(df, aes(x="", y=value, fill=group))+geom_bar(width = 1, stat="identity", color="black")
-bp
+data<-ggplot(df, aes(x="", y=value, fill=group))+geom_bar(width = 1, stat="identity", color="black")
+data
 pie<-bp+coord_polar("y", start=0)+theme_void()
 pie_new<-pie+ scale_fill_brewer(palette="Set2")+guides(fill = guide_legend(title = "Region"))+scale_fill_discrete(breaks=c("Northeast", "South", "Midwest", "West", "Territory"))
 pie_new
